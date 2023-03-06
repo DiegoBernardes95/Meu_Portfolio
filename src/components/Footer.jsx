f16562fc6b9a1633919a7c112631af54cd40910b
Diego Bernardes <113109526+DiegoBernardes95@users.noreply.github.com>
2023-03-06 09:42:35 -0300

    footer atualizado

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,12 +7,12 @@ export default function Footer(){
     return(
         <div className={style.fooTer}>
             <h2>Diego <br /> Bernardes</h2>
-            <ul className={style.listFooter}>
-                <li><a href="#Sobre">Sobre</a></li>
-                <li><a href="#Tecnologias">Tecnologias</a></li>
-                <li><a href="#Projetos">Projetos</a></li>
-                <li><a href="#Contato">Contatos</a></li>
-            </ul>
+            <div className={style.listFooter}>
+                <a href="#Sobre">Sobre</a>
+                <a href="#Tecnologias">Tecnologias</a>
+                <a href="#Projetos">Projetos</a>
+                <a href="#Contato">Contatos</a>
+            </div>
             <h6><strong>©{ano.getFullYear()}</strong></h6>
         </div>
     )
