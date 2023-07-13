a89bbd0f3d587b1edb0d2ce37b5f62d00a94de95
Diego Bernardes <dcb1618@gmail.com>
2023-07-13 19:33:03 -0300

    update projetos

--- src/components/Projetos.jsx
+++ src/components/Projetos.jsx
@@ -10,8 +10,8 @@ const Projetos = () => {
                 </li>
                 <li id="project1" className="projectImg"></li>
                 <li id="description1" className="descriptionProject">
-                    <p>Projeto de clonagem da landing page da Netflix.<br />Feito com HTML, CSS e JavaScript puro para exercitar os conhecimentos básicos nestas tecnologias. <br /></p>
-                    <a href="https://diegobernardes95.github.io/O_Clone/" target="_blank">1_Acessar projeto</a>
+                    <p>Projeto de clonagem da landing page da Netflix.<br />Feito com HTML, CSS e JavaScript puro para exercitar os conhecimentos básicos nestas tecnologias. <br /><br /><a href="https://diegobernardes95.github.io/O_Clone/" target="_blank">1_Acessar projeto</a></p>
+                    
                 </li>
                 {/* SEGUNDO PROJETO */}
                 <li id="title2" className="titleProject">
@@ -19,8 +19,7 @@ const Projetos = () => {
                 </li>
                 <li id="project2" className="projectImg"></li>
                 <li id="description2" className="descriptionProject">
-                    <p>Codificador e Decodificador de Cifra de César e Base64. <br />Projeto desenvolvido no curso "Programadores Cariocas" com a intenção de exercitar a manipulação do DOM.</p>
-                    <a href="https://diegobernardes95.github.io/Projeto-Mensagem-Oculta/" target="_blank">2_Acessar projeto</a>
+                    <p>Codificador e Decodificador de Cifra de César e Base64. <br />Projeto desenvolvido no curso "Programadores Cariocas" com a intenção de exercitar a manipulação do DOM. <br /><br /><a href="https://diegobernardes95.github.io/Projeto-Mensagem-Oculta/" target="_blank">2_Acessar projeto</a></p>
                 </li>
                 {/* TERCEIRO PROJETO */}
                 <li id="title3" className="titleProject">
@@ -28,8 +27,7 @@ const Projetos = () => {
                 </li>
                 <li id="project3" className="projectImg"></li>
                 <li id="description3" className="descriptionProject">
-                    <p>O projeto Sweet Cine foi criado com o intuito de conceder ao usuário um lugar para guardar suas experiências no cinema, catalogando seus filmes assistidos e cinemas frequentados e fornecendo um levantamento sobre os dados armazenados.</p>
-                    <a href="https://github.com/DiegoBernardes95/Sweet_Cine" target="_blank">3_Link do projeto</a>
+                    <p>O projeto Sweet Cine foi criado com o intuito de conceder ao usuário um lugar para guardar suas experiências no cinema, catalogando seus filmes assistidos e cinemas frequentados e fornecendo um levantamento sobre os dados armazenados.<br /><br /><a href="https://github.com/DiegoBernardes95/Sweet_Cine" target="_blank">3_Link do projeto</a></p>
                 </li>
                 {/* QUARTO PROJETO */}
                 <li id="title4" className="titleProject">
@@ -37,8 +35,7 @@ const Projetos = () => {
                 </li>
                 <li id="project4" className="projectImg"></li>
                 <li id="description4" className="descriptionProject">
-                    <p>Projeto desenvolvido no curso "Programadores Cariocas", utilizando HTML & CSS, para a ONG fictícia “Adote Aqui”.</p>
-                    <a href="https://diegobernardes95.github.io/Projeto-Adote-Aqui/" target="_blank">4_Acessar projeto</a>
+                    <p>Projeto desenvolvido no curso "Programadores Cariocas", utilizando HTML & CSS, para a ONG fictícia “Adote Aqui”.<br /><br /><a href="https://diegobernardes95.github.io/Projeto-Adote-Aqui/" target="_blank">4_Acessar projeto</a></p>
                 </li>
             </ul>
         </main>
